f25f3bb3563a9611cca3712f54c87eae283be610
thakurbhanu021 <thakurbhanu021@gmail.com>
2024-01-09 17:04:00 +0530

    layout animate

--- src/components/ChallengeItem.jsx
+++ src/components/ChallengeItem.jsx
@@ -28,7 +28,7 @@ export default function ChallengeItem({
   }
 
   return (
-    <li>
+    <motion.li layout>
       <article className="challenge-item">
         <header>
           <img {...challenge.image} />
@@ -67,6 +67,6 @@ export default function ChallengeItem({
           )}
         </div>
       </article>
-    </li>
+    </motion.li>
   );
 }
